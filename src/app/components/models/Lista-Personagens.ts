8096e6994a43e05b7f3a4cc20fde530c47fe6b15
Edilson Sousa da Silva <edilson.silva@tty2000.com.br>
2024-03-08 14:44:37 -0300

    refactor: refactoring code

---
+++ src/app/components/models/Lista-Personagens.ts
@@ -0,0 +1,31 @@
+
+export interface ApiResponse {
+  info: {
+    count: number;
+    pages: number;
+    next: string | null;
+    prev: string | null;
+  };
+  results: ListaPersonagen[];
+}
+
+export interface ListaPersonagen {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
